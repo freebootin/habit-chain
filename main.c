f4751060a7c9699c8c29434a04709eb34460f832
freebootin <patrick@freebootin.com>
2022-05-30 21:06:53 -0700

    added boilerplate and print_bar function

--- main.c
+++ main.c
@@ -3,18 +3,34 @@
 #include <ncurses.h>
 #include <string.h>
 
+void print_bar(int row, int col, int days);
+
 int main(int argc, char *argv[])
 {
-  char mesg[] = "Habit Chain";
+  char header[] = "Habit Chain";
   int row, col;
 
   initscr();
+  cbreak();
+  noecho();
+  curs_set(0);
+
   getmaxyx(stdscr, row, col);
-  mvprintw(row / 2, (col - strlen(mesg)) / 2, "%s", mesg);
   
+  mvprintw(5, (col - strlen(header)) / 2, "%s", header);
   refresh();
+  print_bar(6, 0, 15);
   getch();
   endwin();
 
   return 0;
+}
+
+void print_bar(int row, int col, int days)
+{
+  move(row, col);
+  for (int i = 0; i < days; i++) {
+    mvaddch(row, col + i, '#');
+    refresh();
+  }
 }
